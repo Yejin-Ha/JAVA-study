52c80978d8d7e5b6822d26a95e6b1934b28ef27a
Yejin-Ha <hyj1004mu@naver.com>
2022-02-01 00:22:49 +0900

    Start Project

---
+++ Data_and_operation/src/DataType.java
@@ -0,0 +1,17 @@
+public class DataType {
+	public static void main(String[] args) {
+		System.out.println(5);	// Number
+		System.out.println("six");	// String
+		
+		System.out.println("6");	// String 6
+		
+		System.out.println(7+7);  // 14
+		System.out.println("7" + "7");  // 77
+		
+		System.out.println(8*8);  // 64
+//		System.out.println("8" * "8");  // error
+		
+		System.out.println("1234".length());  // "1234"의 길이를 반환
+//		System.out.println(1234.length());  // lenth() 는 double 타입에 사용할 수 없다.
+	}
+}
